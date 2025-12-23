7bb0b454bb869b5c34de8cc91dc88c24170d10cf
Levi Bland <arcanefi@tuta.io>
2025-12-23 23:41:31 +1300

    fix: check_pda_seeds_init must return Result<(Pubkey, u8)>

--- crates/pda/src/check_seeds.rs
+++ crates/pda/src/check_seeds.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Arcane Labs <dev@arcane.fi>
 // SPDX-License-Identifier: Apache-2.0
 
-use hayabusa_errors::ErrorCode;
+use hayabusa_errors::{Result, ErrorCode};
 use pinocchio::{
     program_error::ProgramError,
     pubkey::{create_program_address, find_program_address, Pubkey},
@@ -17,16 +17,17 @@ pub trait CheckSeeds {
         &self,
         pk: &Pubkey,
         pda_info: Self::Info<'_>,
-    ) -> Result<(), ProgramError>;
+    ) -> Result<()>;
 
-    fn check_pda_seeds_init(pk: &Pubkey, pda_info: Self::InitInfo<'_>) -> Result<(), ProgramError>;
+    fn check_pda_seeds_init(pk: &Pubkey, pda_info: Self::InitInfo<'_>) -> Result<(Pubkey, u8)>;
 }
 
+// TODO: emit proper errors
 pub fn check_seeds_against_pk(
     seeds: &[&[u8]],
     pk: &Pubkey,
     program_id: &Pubkey,
-) -> Result<(), ProgramError> {
+) -> Result<()> {
     let pda_address = create_program_address(seeds, program_id)?;
 
     if *pk != pda_address {
@@ -40,7 +41,7 @@ pub fn check_seeds_against_pk_no_bump(
     seeds: &[&[u8]],
     pk: &Pubkey,
     program_id: &Pubkey,
-) -> Result<(Pubkey, u8), ProgramError> {
+) -> Result<(Pubkey, u8)> {
     let (pda_address, bump) = find_program_address(seeds, program_id);
 
     if *pk != pda_address {
