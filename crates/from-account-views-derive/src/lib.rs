4af11119bbb0b0e6a19544f039da3def35b263c5
gronksol <arcanefi@tuta.io>
2026-01-14 12:41:44 +1300

    fix: support Mut<T> in FromAccountViews derive, correctly reference the Meta<'a> GAT

--- crates/from-account-views-derive/src/lib.rs
+++ crates/from-account-views-derive/src/lib.rs
@@ -3,17 +3,21 @@
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields, Ident, Type, TypePath};
+use syn::{
+    parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields, Type,
+};
 
 #[proc_macro_derive(FromAccountViews, attributes(meta))]
 pub fn derive_from_account_views(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let struct_name = &input.ident;
-    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let (impl_generics, ty_generics, where_clause) =
+        input.generics.split_for_impl();
 
     // ---- extract exactly one lifetime ('ix)
-    let info_lt = match input.generics.lifetimes().collect::<Vec<_>>().as_slice() {
+    let info_lt = match input.generics.lifetimes().collect::<Vec<_>>().as_slice()
+    {
         [lt] => &lt.lifetime,
         [] => {
             return syn::Error::new(
@@ -60,21 +64,18 @@ pub fn derive_from_account_views(input: TokenStream) -> TokenStream {
 
     for field in fields {
         let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+
         field_idents.push(ident);
 
-        let outer = match outer_type_ident(&field.ty) {
-            Ok(o) => o,
-            Err(e) => return e.to_compile_error().into(),
-        };
-
-        let meta_expr = match parse_meta(&field.attrs, &outer, info_lt) {
+        let meta_expr = match parse_meta(&field.attrs, ty, info_lt) {
             Ok(m) => m,
             Err(e) => return e.to_compile_error().into(),
         };
 
         bindings.push(quote! {
             let #ident =
-                #outer::try_from_account_view(
+                <#ty as FromAccountView<#info_lt>>::try_from_account_view(
                     account_views.next()?,
                     #meta_expr,
                 )?;
@@ -101,29 +102,25 @@ pub fn derive_from_account_views(input: TokenStream) -> TokenStream {
     expanded.into()
 }
 
-fn outer_type_ident(ty: &Type) -> Result<Ident, syn::Error> {
-    match ty {
-        Type::Path(TypePath { path, .. }) => Ok(path.segments.first().unwrap().ident.clone()),
-        _ => Err(syn::Error::new(ty.span(), "expected path type")),
-    }
-}
-
 fn parse_meta(
     attrs: &[syn::Attribute],
-    outer: &Ident,
+    ty: &Type,
     info_lt: &syn::Lifetime,
 ) -> Result<proc_macro2::TokenStream, syn::Error> {
     for attr in attrs {
         if attr.path().is_ident("meta") {
             let args = attr.parse_args_with(
-                syn::punctuated::Punctuated::<syn::MetaNameValue, syn::Token![,]>::parse_terminated,
+                syn::punctuated::Punctuated::<
+                    syn::MetaNameValue,
+                    syn::Token![,],
+                >::parse_terminated,
             )?;
 
-            // Named args only; we pass VALUES in declaration order
+            // Named args only; values are passed in declaration order
             let values = args.iter().map(|kv| &kv.value);
 
             return Ok(quote! {
-                <#outer as FromAccountView<#info_lt>>::Meta::new(
+                <#ty as FromAccountView<#info_lt>>::Meta<'_>::new(
                     #(#values),*
                 )
             });
